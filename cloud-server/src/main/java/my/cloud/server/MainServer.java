b033bd6f81b1c8d1a939f85230e95d4ed17cd8d3
Anton Komissarov <komissarov1988@gmail.com>
2021-05-06 20:42:47 +0500

    remove scanner

--- cloud-server/src/main/java/my/cloud/server/MainServer.java
+++ cloud-server/src/main/java/my/cloud/server/MainServer.java
@@ -9,20 +9,6 @@ public class MainServer {
 
     public static void main(String[] args) {
         Migrations.migrate();
-
-        new Thread(() -> {
-            Factory.getServerService().startServer();
-        }).start();
-
-        Logger.info("Server started. Print \"stop\" to quit");
-        Scanner scanner = new Scanner(System.in);
-        while(true) {
-            String s = scanner.nextLine();
-            if (s.equals("stop")) {
-                Logger.info("stopping server...");
-                Factory.getServerService().stopServer();
-                break;
-            }
-        }
+        Factory.getServerService().startServer();
     }
 }
