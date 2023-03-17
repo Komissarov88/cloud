bbe05c08f1614c416d5fd62a4f001c0f2ddacbcf
Anton Komissarov <akomissarov@fil-it.ru>
2023-03-17 10:37:14 +0300

    add final to connection config

--- cloud-server/src/main/java/my/cloud/server/Migrations.java
+++ cloud-server/src/main/java/my/cloud/server/Migrations.java
@@ -5,16 +5,14 @@ import utils.PropertiesReader;
 
 public class Migrations {
 
-    private static int PORT = Integer.parseInt(PropertiesReader.getProperty("db.port"));
-    private static String HOST = PropertiesReader.getProperty("db.address");
-    private static String DB = PropertiesReader.getProperty("db.name");
-    private static String USER = PropertiesReader.getProperty("db.user");
-    private static String PWD = PropertiesReader.getProperty("db.password");
+    private static final int PORT = Integer.parseInt(PropertiesReader.getProperty("db.port"));
+    private static final String HOST = PropertiesReader.getProperty("db.address");
+    private static final String DB = PropertiesReader.getProperty("db.name");
+    private static final String USER = PropertiesReader.getProperty("db.user");
+    private static final String PWD = PropertiesReader.getProperty("db.password");
 
     public static void migrate() {
-
         String url = "jdbc:postgresql://" + HOST + ":" + PORT + "/" + DB;
-
         Flyway flyway = Flyway.configure()
                 .mixed(true)
                 .dataSource(url, USER, PWD).load();
